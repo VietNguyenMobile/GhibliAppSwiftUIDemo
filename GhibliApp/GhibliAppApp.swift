d5f516da7bf95b26dff47f39be02a7a6b354bd43
Viet Nguyen <>
2026-05-02 22:14:12 +0700

    init project

---
+++ GhibliApp/GhibliAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliAppApp.swift
+//  GhibliApp
+//
+//  Created by Macbook on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct GhibliAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
